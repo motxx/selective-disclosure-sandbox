3abad09495661f1fd7416e644a2a7e0d81842f97
motxx <motohiko.ave@gmail.com>
2023-06-26 03:26:19 +0900

    improve verifier ui

--- lit-protocol-app/usecase/verifier.ts
+++ lit-protocol-app/usecase/verifier.ts
@@ -1,7 +1,14 @@
-export const createPresentation = async () => {
-  return 'presentation created';
+import { Web3 } from '~/lib/web3';
+
+export const verifyPresentation = async (presentation: any) => {
+  console.log('presentation', presentation);
+  return true;
 };
 
-export const execute = async () => {
-  console.log('dApp executed');
+export const downloadPresentation = async () => {
+  const web3 = await Web3.connectWallet();
+  const verifierAddress = await web3.getAddress();
+  const key = `presentation-${verifierAddress}`;
+  const presentation = window.localStorage.getItem(key);
+  return presentation ?? '';
 };
