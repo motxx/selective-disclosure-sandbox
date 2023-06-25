e36468fd5d4e9d125b975dd38cc5699e56ef25a0
motxx <motohiko.ave@gmail.com>
2023-06-25 15:26:34 +0900

    feat: add base58 keypair generator

--- lit-protocol-app/lib/credentials-bbs.ts
+++ lit-protocol-app/lib/credentials-bbs.ts
@@ -1,5 +1,11 @@
+import BIP32Factory from 'bip32';
+import * as ecc from 'tiny-secp256k1';
+import * as bip39 from 'bip39';
+import * as bs58 from 'bs58';
 import { CredentialKeyPair } from './types/credentials';
 import { Web3 } from './web3';
+
+const bip32 = BIP32Factory(ecc);
 
 export class CredentialsBBS {
   constructor(private web3: Web3) {}
@@ -12,8 +18,23 @@ export class CredentialsBBS {
     };
   }
 
-  async generateBase58KeyPair() {
-    const message = 'base58 private key seed generator';
-    const seed = await this.web3.sign(message);
+  async generateBase58KeyPair(): Promise<CredentialKeyPair> {
+    const message =
+      'BASE58 PRIVATE KEY SEED GENERATOR. DO NOT PUBLISH DERIVED SIGNATURE!';
+    const seedPhrase = await this.web3.sign(message);
+    const mnemonic = bip39.entropyToMnemonic(seedPhrase);
+    const seed = bip39.mnemonicToSeedSync(mnemonic);
+    const master = bip32.fromSeed(seed);
+    const keyPair = master.derivePath('m/0/0');
+    if (!keyPair.privateKey) {
+      throw new Error('Private key not found');
+    }
+    if (!keyPair.publicKey) {
+      throw new Error('Public key not found');
+    }
+    return {
+      privateKeyBase58: bs58.encode(keyPair.privateKey),
+      publicKeyBase58: bs58.encode(keyPair.publicKey),
+    };
   }
 }
